e7476c3ac77d421637fb35efb383e516889d0376
Miha Jakovac <miha@mjc.si>
2020-11-16 10:38:43 +0100

    old updates

---
+++ src/QAToolKit.Engine.HttpTester.Test/Exceptions/QAToolKitEngineHttpTesterExceptionTests.cs
@@ -0,0 +1,27 @@
+﻿using QAToolKit.Engine.HttpTester.Exceptions;
+using System;
+using Xunit;
+
+namespace QAToolKit.Engine.HttpTester.Test.Exceptions
+{
+    public class QAToolKitEngineHttpTesterExceptionTests
+    {
+        [Fact]
+        public void CreateExceptionTest_Successful()
+        {
+            var exception = new QAToolKitEngineHttpTesterException("my error");
+
+            Assert.Equal("my error", exception.Message);
+        }
+
+        [Fact]
+        public void CreateExceptionWithInnerExceptionTest_Successful()
+        {
+            var innerException = new Exception("Inner");
+            var exception = new QAToolKitEngineHttpTesterException("my error", innerException);
+
+            Assert.Equal("my error", exception.Message);
+            Assert.Equal("Inner", innerException.Message);
+        }
+    }
+}
